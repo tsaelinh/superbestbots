6901d56d9778c5857458be634c23a44aaf9f9a76
Tiffany Saelinh <saelinht@gmail.com>
2018-01-21 17:45:53 -0800

    removing all extra comments and commands that I don't use

--- superbestbots.py
+++ superbestbots.py
@@ -24,28 +24,10 @@ async def add(left : int, right : int):
     """Adds two numbers together."""
     await bot.say(left + right)
 
-@bot.command()
-async def roll(dice : str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await bot.say('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await bot.say(result)
-
 @bot.command(description='For when you wanna settle the score some other way')
 async def choose(*choices : str):
     """Chooses between multiple choices."""
     await bot.say(random.choice(choices))
-
-@bot.command()
-async def repeat(times : int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await bot.say(content)
 
 @bot.command()
 async def joined(member : discord.Member):
@@ -70,21 +52,10 @@ async def _limp():
     """Is limp_noodle cool?"""
     await bot.say('Yes, limp_noodle is always cool.')
 
-#@bot.event
-#async def friends(message):
-#    if message.content.startswith('let\'s be friends'):
-#        await bot.send_message(message.channel, 'yes, let\'s be super best friends')
-
-#@bot.event
-#async def can_i(message):
-#    if "can i" in message.content:
-#        await bot.send_message(message.channel, 'no')
-
 @bot.event
 async def on_message(message):
     if "nooo" in message.content:
         await bot.send_message(message.channel, 'VEGETA YES!')
-#        await bot.process_commands(message)
     if "christmas" in message.content:
         await bot.send_message(message.channel, 'Christmas is about friendship and family and KILLING SANTA')
     if "can i" in message.content:
